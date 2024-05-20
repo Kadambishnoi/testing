0dcb8612b25159f9c8b3523f0decd85d8298cf12
Kadam Bishnoi <kadamgodara29@gmail.com>
2024-05-20 08:57:06 +0530

    tesing pahse 2

--- app/src/main/java/com/example/testing/MainActivity.java
+++ app/src/main/java/com/example/testing/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.testing;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +21,9 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        getdata();
+    }
+    public void getdata(){
+        Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
     }
 }
